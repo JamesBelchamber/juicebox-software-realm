a1a88f962b9dd291f1a63592189325d062fde2e7
James Belchamber <james@belchamber.com>
2025-06-13 13:00:03 +0000

    Removing service from modules

--- gcp/main.tf
+++ gcp/main.tf
@@ -3,20 +3,6 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_project_service" "cloud_run" {
-  service = "run.googleapis.com"
-}
-
-resource "google_project_service" "secrets_manager" {
-  project = var.project_id
-  service = "secretmanager.googleapis.com"
-}
-
-resource "google_project_service" "pub_sub" {
-  project = var.project_id
-  service = "pubsub.googleapis.com"
-}
-
 resource "google_service_account" "service_account" {
   account_id   = "jb-sw-realms-${random_string.suffix.id}"
   display_name = "Juicebox Software Realms (${random_string.suffix.id})"
