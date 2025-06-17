f3eb65f07d10a59c0dd188b18a10e356988f64e8
James Belchamber <james.belchamber@triton.one>
2025-06-17 15:23:16 +0000

    Re-removing provider

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_service_account" "service_account" {
   account_id   = "jb-sw-realms"
   display_name = "Juicebox Software Realms"
